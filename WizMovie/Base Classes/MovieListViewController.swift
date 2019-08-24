949dd034943e78e7df7f3be6e5b6c2c48bd1ec71
Shaikh Farooq <farooq@75f.io>
2019-08-24 18:11:57 +0530

    Completed UI design for Favorite and Movie Details screen

--- WizMovie/Base Classes/MovieListViewController.swift
+++ WizMovie/Base Classes/MovieListViewController.swift
@@ -33,7 +33,7 @@ extension MovieListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 5
+      return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
