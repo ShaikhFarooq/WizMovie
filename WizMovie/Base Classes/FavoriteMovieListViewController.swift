9ff9f67d51d0165779ff55aeeb876bc9aea85a06
Shaikh Farooq <farooq@75f.io>
2019-08-26 00:46:16 +0530

    Added favorite view controllerclass

---
+++ WizMovie/Base Classes/FavoriteMovieListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  FavoriteMovieListViewController.swift
+//  WizMovie
+//
+//  Created by Admin on 8/24/19.
+//  Copyright © 2019 Farooq. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteMovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+// MARK: UITableViewDataSource
+
+extension FavoriteMovieListViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let movieCell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as? MovieCell else {
+            fatalError("Invalid cell Identifier! Check `MovieRowCell` identifier function.")
+        }
+        return movieCell
+        
+    }
+    
+}
+
+extension FavoriteMovieListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 125
+    }
+    
+}
