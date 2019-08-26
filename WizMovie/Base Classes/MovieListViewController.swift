c9d87bf6f1402e094f9d3a3ca01eba15dce0bd83
Shaikh Farooq <farooq@75f.io>
2019-08-27 01:50:53 +0530

    Data binding from viewmodel to controller and custom movie cell and also added some attributes to make search more user friendly

--- WizMovie/Base Classes/MovieListViewController.swift
+++ WizMovie/Base Classes/MovieListViewController.swift
@@ -10,20 +10,43 @@ import UIKit
 
 class MovieListViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    
     @IBOutlet weak var mTableView: UITableView!{
         didSet {
             mTableView.tableFooterView = UIView(frame: .zero)
         }
     }
     
+    // MARK: - Injection
+    
+    let moviewViewModel = MovieViewModel(network: Network())
+    
+    
+    // MARK: - Life Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setUpSearchBar()
+        fetchMovieList(movieTile: "bad")
     }
 
+     // MARK: - Networking
+    
+    func fetchMovieList(movieTile: String){
+        moviewViewModel.searchMovieWithTitle(movieTitle: movieTile){ [weak self] in
+            DispatchQueue.main.async {
+                self?.mTableView.reloadData()
+            }
+        }
+    }
+    
     func setUpSearchBar(){
         navigationItem.searchController = UISearchController(searchResultsController: nil)
+        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationItem.searchController?.dimsBackgroundDuringPresentation = false
+        navigationItem.searchController?.searchBar.sizeToFit()
         navigationItem.hidesSearchBarWhenScrolling = false
         navigationItem.searchController?.searchBar.placeholder = "Search by movie name"
         navigationItem.searchController?.searchBar.delegate = self
@@ -32,12 +55,44 @@ class MovieListViewController: UIViewController {
 
 }
 
-// MARK: UISearchBar Delegate
+// MARK: UITableView DataSource Methods
+
+extension MovieListViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return moviewViewModel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let movieCell = tableView.dequeueReusableCell(withIdentifier: MovieCell.reuseIdentifier,
+                                                       for: indexPath) as? MovieCell else {
+                                                        return UITableViewCell()
+        }
+        
+        let cellViewModel = moviewViewModel.cellViewModel(index: indexPath.row)
+        movieCell.viewModel = cellViewModel
+        return movieCell
+    }
+    
+}
+
+// MARK: UITableView Delegate Methods
+
+extension MovieListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 125
+    }
+}
+
+// MARK: SearchBar Delegate Methods
 
 extension MovieListViewController:  UISearchBarDelegate{
     func searchBar(_ searchBar: UISearchBar, textDidChange imdbTitle:String) {
-        print(imdbTitle)
+        fetchMovieList(movieTile: imdbTitle)
     }
-
 }
 
