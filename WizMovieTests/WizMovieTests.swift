f3e0d66989a0819f202aef1712bf55c6729abd73
Shaikh Farooq <farooq@75f.io>
2019-08-31 23:28:53 +0530

    added few test cases like tile search api ,messages and network connection

--- WizMovieTests/WizMovieTests.swift
+++ WizMovieTests/WizMovieTests.swift
@@ -12,7 +12,7 @@ import XCTest
 class WizMovieTests: XCTestCase {
 
     private let networkService = Network.shared
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -35,24 +35,96 @@ class WizMovieTests: XCTestCase {
 
     func testSearchWithMovieAvailable() {
         let promise = expectation(description: "Search for batman movies")
-        let url: String = "\(EndPoints.Search.path)\("batman")\(APIKey.ApiKey.rawValue)"
+        let movieTitle = "batman"
+        guard let movTitle =         movieTitle.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)else{
+            return
+        }
+        let url: String = "\(EndPoints.Search.path)\(movTitle)\(APIKey.ApiKey.rawValue)"
         networkService.searchMoviesOnJson(urlByName: url,type: Movies.self) { (response,success,error) in
             XCTAssertNil(error)
             XCTAssertTrue(success == "True")
             promise.fulfill()
         }
-        waitForExpectations(timeout: 2, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
     }
     
     func testSearchNoMovieAvailable() {
         let promise = expectation(description: "Search for ba movies")
-        let url: String = "\(EndPoints.Search.path)\("ba")\(APIKey.ApiKey.rawValue)"
+        let movieTitle = "ba"
+        guard let movTitle =         movieTitle.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)else{
+            return
+        }
+        let url: String = "\(EndPoints.Search.path)\(movTitle)\(APIKey.ApiKey.rawValue)"
         networkService.searchMoviesOnJson(urlByName: url,type: Movies.self) { (response,success,error) in
             XCTAssertTrue(success == "False")
             XCTAssertEqual("The operation couldn’t be completed. (No Movie error -101.)",
                            error?.localizedDescription)
             promise.fulfill()
         }
-        waitForExpectations(timeout: 2, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
     }
+    
+    func testMovieWithTitle(){
+        let promise = expectation(description: "Search for City of God movies")
+        let movieTitle = "City of God"
+        guard let movTitle =         movieTitle.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)else{
+            return
+        }
+        let url: String = "\(EndPoints.Title.path)\(movTitle)\(APIKey.ApiKey.rawValue)"
+        print(url)
+        networkService.searchMoviesOnJson(urlByName: url,type: MovieDetails.self) { (response,success,error) in
+            XCTAssertNil(error)
+            XCTAssertTrue(success == "True")
+            promise.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testMovieWithTitleNotAvailable() {
+        let promise = expectation(description: "Search for ba movies")
+        let movieTitle = "raju cha"
+        guard let movTitle =         movieTitle.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)else{
+            return
+        }
+        let url: String = "\(EndPoints.Title.path)\(movTitle)\(APIKey.ApiKey.rawValue)"
+        print(url)
+        networkService.searchMoviesOnJson(urlByName: url,type: MovieDetails.self) { (response,success,error) in
+            XCTAssertTrue(success == "False")
+            XCTAssertEqual("The operation couldn’t be completed. (No Movie error -101.)",
+                           error?.localizedDescription)
+            promise.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    
+    func testNetworkConnectionWhenOffline(){
+        XCTAssertFalse(NetworkConnection.isConnectedToNetwork())
+    }
+    
+    func testNetworkConnectionWhenOnline(){
+        XCTAssertTrue(NetworkConnection.isConnectedToNetwork())
+    }
+    
+    func testCheckNoFavoriteMoviesMessge(){
+        XCTAssertEqual("No Favorite Movies Found",noFavMovies)
+    }
+    
+    func testCheckNoNetworkMessge(){
+        XCTAssertEqual("Network Error”, “Unable to contact the server.",networkError)
+    }
+    
+    func testCheckLoadingMessage(){
+        XCTAssertEqual("Please wait....",loadingMessage)
+    }
+    
+    func testCheckSearchMessge(){
+        XCTAssertEqual("Search your favorite movies.",searchMessage)
+    }
+    
+    func testCheckNoMovieMessge(){
+        let movieName = "ba"
+        XCTAssertEqual("No Movies Found for ba","\(noMovieFound )\(movieName)")
+    }
+    
 }
