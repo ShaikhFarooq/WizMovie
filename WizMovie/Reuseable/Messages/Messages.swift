7ee350b9df42a539a3e82dd8d5537808edc951fd
Shaikh Farooq <farooq@75f.io>
2019-08-31 23:21:27 +0530

    removed messages from viewmodel and make it common inorder to write test case and reuse them

---
+++ WizMovie/Reuseable/Messages/Messages.swift
@@ -0,0 +1,15 @@
+//
+//  Messages.swift
+//  WizMovie
+//
+//  Created by Admin on 8/31/19.
+//  Copyright © 2019 Farooq. All rights reserved.
+//
+
+import Foundation
+
+let noFavMovies: String = "No Favorite Movies Found"
+let loadingMessage: String = "Please wait...."
+let searchMessage: String = "Search your favorite movies."
+let noMovieFound: String = "No Movies Found for "
+let networkError: String = "Network Error”, “Unable to contact the server."
